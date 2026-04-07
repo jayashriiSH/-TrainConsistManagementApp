94ed990a12d4dd6db4f9081fd06f6145834ab778
jayashriiSH <pcmj.jayashrii@gmail.com>
2026-04-07 08:26:38 +0530

    Base application setup

---
+++ src/TrainConsistManagementApp.java
@@ -0,0 +1,5 @@
+public class TrainConsistManagementApp {
+}
+git config --global user.email "pcmj.jayashrii@gmail.com"
+
+git remote add origin https://github.com/jayashriiSH/-TrainConsistManagementApp.git
